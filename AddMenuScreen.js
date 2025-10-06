4dc06b8adc89f784d4333de665f180e4e25f9f5e
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-10-06 16:49:00 +0200

    add state variables for menu input fields
    variables to store the dish name, description, price, and course. It allows the chef to type in and update these details when adding a new menu item.

--- AddMenuScreen.js
+++ AddMenuScreen.js
@@ -2,4 +2,15 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
-export default function AddMenuScreen({ navigation, menuItems, setMenuItems }) {}
+export default function AddMenuScreen({ navigation, menuItems, setMenuItems }) {
+const [dishName, setDishName] = useState('');
+  const [dishDescription, setDishDescription] = useState('');
+  const [dishPrice, setDishPrice] = useState('');
+  const [course, setCourse] = useState('Starter');
+
+
+
+
+
+
+}
