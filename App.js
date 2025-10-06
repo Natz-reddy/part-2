38dbdff60612791a33fdac320e3c796df365df67
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-10-06 16:14:28 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './HomeScreen';
+import AddMenuScreen from './AddMenuScreen';
+
