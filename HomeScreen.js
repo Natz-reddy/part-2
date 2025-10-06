c84dead0f089930dedd45f9b8a7cf5bdcbbeb1b9
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-10-06 16:27:28 +0200

    adding new
    added a const for the number of menu items to store.
    return on the style for the app, text as title and subtitle for the name and total number of dishes

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function HomeScreen({ navigation, menuItems }) {}
+export default function HomeScreen({ navigation, menuItems }) {
+  const totalItems = menuItems.length;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Christoffel's Menu </Text>
+      <Text style={styles.subtitle}>Total Dishes: {totalItems}</Text>
+      )
+
+
+}
