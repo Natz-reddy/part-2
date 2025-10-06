9433ed3d81e4a47fea882c0b2bc4e53e98f13e21
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-10-06 16:45:26 +0200

    import new function

---
+++ AddMenuScreen.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+export default function AddMenuScreen({ navigation, menuItems, setMenuItems }) {}
