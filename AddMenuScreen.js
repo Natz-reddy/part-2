0526ecdddf8e381c2cb8dc0cf05e6538db693cd3
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-10-06 16:59:35 +0200

    adding style to the page

--- AddMenuScreen.js
+++ AddMenuScreen.js
@@ -80,3 +80,13 @@ const [dishName, setDishName] = useState('');
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, backgroundColor: '#d8cfc4' },
+  title: { fontSize: 22, fontWeight: 'bold', marginBottom: 15, textAlign: 'center' },
+  input: { borderWidth: 1, borderColor: '#ccc', borderRadius: 8, padding: 10, marginBottom: 12, backgroundColor: '#fff', color: '#000' },
+  picker: { marginBottom: 16, backgroundColor: '#fff', height: 100 },
+  button: { backgroundColor: '#77dd77', padding: 12, borderRadius: 8, alignItems: 'center', marginBottom: 20 },
+  buttonText: { color: '#fff', fontSize: 16, fontWeight: 'bold' },
+  card: { backgroundColor: '#fff', borderRadius: 10, padding: 15, marginBottom: 10 },
+  name: { fontSize: 18, fontWeight: 'bold', marginBottom: 4 },
+});
