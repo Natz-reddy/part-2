5ce1958cf92b87d20cc3139da4d5f1ad2f5d20ca
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-11-10 17:52:39 +0200

    editing code
    making slight edits to imports of my code to link the filter menu screen properly and to get the menu to show up

--- App.js
+++ App.js
@@ -1,51 +1,33 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import AddMenuScreen from './AddMenuScreen';
 import FilterMenuScreen from './FilterMenuScreen';
 
-const Stack = createNativeStackNavigator();
-
-const predefinedMenu = [
-  { name: 'Tomato Soup', description: 'Classic starter', course: 'Starters', price: 45 },
-  { name: 'Grilled Chicken', description: 'Served with veggies', course: 'Mains', price: 120 },
-  { name: 'Chocolate Mousse', description: 'Rich and creamy', course: 'Dessert', price: 60 },
-];
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [menuItems, setMenuItems] = useState(predefinedMenu);
+  const [menuItems, setMenuItems] = useState([]);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home">
           {(props) => (
-            <HomeScreen
-              {...props}
-              menuItems={menuItems}
-              setMenuItems={setMenuItems}
-            />
+            <HomeScreen {...props} menuItems={menuItems} setMenuItems={setMenuItems} />
           )}
         </Stack.Screen>
 
         <Stack.Screen name="AddMenu">
           {(props) => (
-            <AddMenuScreen
-              {...props}
-              menuItems={menuItems}
-              setMenuItems={setMenuItems}
-            />
+            <AddMenuScreen {...props} menuItems={menuItems} setMenuItems={setMenuItems} />
           )}
         </Stack.Screen>
 
         <Stack.Screen name="FilterMenu">
           {(props) => (
-            <FilterMenuScreen
-              {...props}
-              menuItems={menuItems}
-            />
+            <FilterMenuScreen {...props} menuItems={menuItems} />
           )}
         </Stack.Screen>
       </Stack.Navigator>
